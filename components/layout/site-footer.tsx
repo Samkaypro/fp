069c76679d5dfb03e795d68db26638e0226ab3a4
samkaypro <samkayode057@gmail.com>
2025-03-06 17:04:57 +0100

    update

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -66,6 +66,7 @@ export function SiteFooter() {
               <div className="flex items-center gap-2 opacity-90">
                 <Phone className="size-5" />
                 <span>+234 803 502 5879</span>
+                <span>+234 707 124 9966</span>
               </div>
               <div className="flex items-start gap-2 opacity-90">
                 <MapPin className="size-5 shrink-0" />
